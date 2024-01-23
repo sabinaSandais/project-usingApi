b76819356f7f612eff8763ac97b28a8f130c6d2c
Sabina84 <129327140+Sabina84@users.noreply.github.com>
2024-01-23 23:55:55 +0100

    Updated files
    App completed

--- recipeApi.js
+++ recipeApi.js
@@ -1,6 +1,6 @@
 
 export const fetchRecipes = async (query) => {
-  const recipeApiKey = `https://api.edamam.com/search?q=${query}&app_id=19bde2bf&app_key=e66d79d01e5f7b88d13643b3f5f2861c&to=15`;
+  const recipeApiKey = ``;
   
   try {
     const data = await fetch(
